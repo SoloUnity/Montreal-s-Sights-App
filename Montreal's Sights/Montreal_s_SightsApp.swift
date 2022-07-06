1d6c19b5586a15a22c4aa41c4cb723ae59e5161f
SoloUnity <GordonAccount@yahoo.com>
2022-07-06 10:57:00 -0400

    Initial Commit

---
+++ Montreal's Sights/Montreal_s_SightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Montreal_s_SightsApp.swift
+//  Montreal's Sights
+//
+//  Created by Gordon Ng on 2022-07-06.
+//
+
+import SwiftUI
+
+@main
+struct Montreal_s_SightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
