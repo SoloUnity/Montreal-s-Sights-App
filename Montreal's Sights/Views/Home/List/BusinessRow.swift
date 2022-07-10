4633e7ead8745105c7e4ac1435066794e5ac67f6
SoloUnity <GordonAccount@yahoo.com>
2022-07-10 12:04:27 -0400

    Fix layout

--- Montreal's Sights/Views/Home/List/BusinessRow.swift
+++ Montreal's Sights/Views/Home/List/BusinessRow.swift
@@ -9,37 +9,42 @@ import SwiftUI
 
 struct BusinessRow: View {
     
-    // Observed to wait for image, business row reacts when image is received
     @ObservedObject var business: Business
     
     var body: some View {
-        VStack(alignment: .leading){
-            // Image
-            let uiImage = UIImage(data: business.imageData ?? Data())
-            Image(uiImage: uiImage ?? UIImage())
-                .resizable()
-                .frame(width:50, height: 50)
-                .cornerRadius(10)
-                .scaledToFit()
+        
+        VStack (alignment: .leading) {
             
-            // Name and distance
-            HStack{
-                VStack(alignment: .leading){
+            HStack {
+                // Image
+                let uiImage = UIImage(data: business.imageData ?? Data())
+                Image(uiImage: uiImage ?? UIImage())
+                    .resizable()
+                    .frame(width:58, height:58)
+                    .cornerRadius(5)
+                    .scaledToFit()
+                
+                // Name and distance
+                VStack (alignment: .leading) {
                     Text(business.name ?? "")
                         .bold()
-                    Text(String(format:"%.1f km away", (business.distance ?? 0)/1000))
+                    
+                    Text(String(format:"%.1f km away", (business.distance ?? 0)/1000 ))
+                        .font(.caption)
+                }
+                
+                Spacer()
+                
+                // Star rating and number of reviews
+                VStack (alignment: .leading) {
+                    Image("regular_\(business.rating ?? 0)")
+                    Text("\(business.reviewCount ?? 0) Reviews")
                         .font(.caption)
                 }
             }
             
-            Spacer()
-            
-            // Star rating and number of reviews
-            VStack(alignment: .leading){
-                Image("regular_\(business.rating ?? 0)")
-                Text("\(business.reviewCount ?? 0) reviews")
-                    .font(.caption)
-            }
+            Divider()
         }
+        
     }
 }
